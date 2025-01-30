317060ef8e499554759f76fca7c1703353e9d709
LeandroKlaus <leklaussl@gmail.com>
2025-01-30 01:09:00 -0400

    Deu errado akakakakak, mas vou tentar outra coisa aqui

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './fonts/fonts.css'; // Importar o CSS da fonte
 
 ReactDOM.render(
     <React.StrictMode>
